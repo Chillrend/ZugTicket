27d6fa7534973c15f6649a7e85c633f6f66876ab
Farhan Hanif <cv2lex@gmail.com>
2018-05-15 07:52:21 +0700

    Added Sources Files ( JS, CSS and Images )
    New Files

---
+++ source/js/main.js
@@ -0,0 +1,6 @@
+ $(document).ready(function () {
+     $('.mdb-select').material_select();
+     $('.dropdown-toggle').dropdown()
+ });
+ // Data Picker Initialization
+ $('.datepicker').pickadate();
