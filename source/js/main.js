cf3d8905670fba6ef329bb9b1f7b0b608e159f65
Farhan Hanif <cv2lex@gmail.com>
2018-05-16 09:11:33 +0700

    Added a Bahn Pass Card
    fixed the navbar dropdown issues and added a card to show train pass, footer later

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
  $(document).ready(function () {
      $('.mdb-select').material_select();
-     $('.dropdown-toggle').dropdown()
+     $('.dropdown-toggle').dropdown();
+     $('.datepicker').pickadate();
  });
- // Data Picker Initialization
- $('.datepicker').pickadate();
+ 
+
